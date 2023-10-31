e04c3f883a1f29a4a775dbf6e731623dfaef3a55
NumberMedal9866 <sstsoy1616@gmail.com>
2023-10-31 16:32:01 +0500

    sdf

--- src/pages/Contact/Contact.vue
+++ src/pages/Contact/Contact.vue
@@ -21,11 +21,11 @@
                     </div>
                     <div class="message-main-input">
                         <span>Name:</span>
-                        <input type="text" placeholder="Enter your name" name="name">
+                        <input type="text" placeholder="Enter your name" required name="name">
                     </div>
                     <div class="message-main-input">
                         <span>Number:</span>
-                        <input type="number" placeholder="Enter your phone nubmer" name="number">
+                        <input type="number" placeholder="Enter your phone nubmer" required name="number">
                     </div>
                     <textarea id="1" cols="30" rows="10" placeholder="Write your message here" required name="message"></textarea>
                     <button type="submit">Send</button>
@@ -42,15 +42,19 @@ import emailjs from '@emailjs/browser';
 const form = ref(null);
 
 const handleSubmit = () => {
-    emailjs.sendForm('service_g5afzjp', 'template_dbx3kjh', form.value, 'D9IhY7x-ylH7EhbZQ')
-      .then((result) => {
-        console.log('SUCCESS!', result.text);
-        alert("Your From has been submitted, please wait for me to get in touch with you")
-      })
-      .catch((error) => {
-        console.log('FAILED...', error.text);
-      });
-  }
+    setTimeout(() => {
+        emailjs.sendForm('service_g5afzjp', 'template_dbx3kjh', form.value, 'D9IhY7x-ylH7EhbZQ')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+            alert("Your From has been submitted, please wait for me to get in touch with you")
+        })
+        .catch((error) => {
+            console.log('FAILED...', error.text);
+        });
+        // form.value=''
+        // window.location.reload()
+    }, 2000);
+}
 </script>
 
 <style lang="scss" scoped>
